2442913fbaefce1fa521a2f83f73b130037eb08a
kris <kris@KRIS_PC>
2016-10-29 17:09:15 +0200

    changed size of picturebox

--- Game_of_life/Form1.Designer.cs
+++ Game_of_life/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(884, 581);
+            this.pictureBox1.Size = new System.Drawing.Size(880, 580);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 

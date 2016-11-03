25150f3e8254ffe0c9eddeffda8ab4a2556135b9
kris <kris@KRIS_PC>
2016-11-03 18:35:31 +0100

    added class amoebe

--- Game_of_life/Form1.cs
+++ Game_of_life/Form1.cs
@@ -16,5 +16,10 @@ namespace Game_of_life
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Game_of_life/amoebe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_life
+{
+    class amoebe
+    {
+        public Int32 name;
+        public Int32 age;
+        public Int32 xPos;
+        public Int32 yPos;
+        public String sex;
+        public Boolean furtile = true;
+
+    }
+}
